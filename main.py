fb10b495e0122f50488a9d6b642630eddca48619
Siddharth Saran <siddharthsaran@MacBook-Pro-254.local>
2025-07-13 18:07:44 -0700

    initial implementation of best 3 environments

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
-from math import inf
 import pandas as pd
 
 TAX = pd.read_csv("Datasets/tax.csv")
 CULTURE = pd.read_csv("Datasets/cultureProportions_Env.csv")
 MICROBIOME = pd.read_csv("Datasets/microbiomeProportions_Env.csv")
 
-def find_initial_environment(culture, bacteria):
+def find_next_environment(culture, bacteria):
     bacteria_row = culture.loc[bacteria]
-    initial_max = bacteria_row.max()
-    mask = bacteria_row == initial_max
-    highest_initial = bacteria_row[mask]
-    inital_culture = None
-    if len(highest_initial) > 1:
-        max_temp = float(inf)
-        for name in highest_initial.index:
-            sum = culture[name].sum() - initial_max
-            if sum < max_temp:
-                initial_culture = name
-                max_temp = sum
-    else:
-        initial_culture = bacteria_row.idxmax()
-    return initial_culture
+    return bacteria_row.idxmax()
 
 
 def find_n_sequences(bacteria, num_sequences):
     environments = []
     culture = CULTURE.set_index("OTU")
-    environments.append(find_initial_environment(culture, bacteria))
+    for _ in range(int(num_sequences)):
+        curr_env = find_next_environment(culture, bacteria)
+        environments.append(curr_env)
+        culture = culture.drop(columns=curr_env)
     return environments
 
 def main():
     bacteria = input("Type a bacteria: ")
-    num_sequences = input("Type the number of environments you want to grow ")
+    num_sequences = input("Type the number of environments you want to grow: ")
 
     print(find_n_sequences(bacteria, num_sequences))
     
